c69f413cd24d4c0f04e9c042c45b9c90c015f0c2
sannawag <scwager@indiana.edu>
2016-12-10 11:14:57 -0500

    loss decreases, need to see whether prediction error improves with more iterations

--- neuralNet.py
+++ neuralNet.py
@@ -8,8 +8,9 @@ import matplotlib.pyplot as plt
 
 '''
 http://www.wildml.com/2015/09/implementing-a-neural-network-from-scratch/
-#http://iamtrask.github.io/2015/07/12/basic-python-network/
+http://iamtrask.github.io/2015/07/12/basic-python-network/
 http://ufldl.stanford.edu/tutorial/supervised/MultiLayerNeuralNetworks/
+http://axon.cs.byu.edu/papers/Wilson.nn03.batch.pdf
 '''
 
 class NeuralNet:
@@ -46,8 +47,12 @@ class NeuralNet:
         angles = np.array([angle//90 for (id, angle) in imgIds.values()])
         return self.oneHotIt(angles)
         
+    def softmax_batch(self, z):
+#         z -= np.max(z)
+        return (np.exp(z).T / np.sum(np.exp(z),axis=1)).T
+
     def softmax(self, z):
-        z -= np.max(z)
+#         z -= np.max(z)
         return np.exp(z) / np.sum(np.exp(z))
 
     def activation_delta(self, a, type):
@@ -95,8 +100,29 @@ class NeuralNet:
         return loss,grad
               
     def getLoss(self, output,y,dim):
-        prob = self.softmax(output) #get probability distribution
+#         prob = self.softmax(output) #get probability distribution
+        prob = output
         return np.sum( np.power(prob - y,2) ) / dim
+        
+    def getLoss_training(self, x, y, w1, w2, b1, b2):
+        '''calculate loss for all examples combined in a batch'''
+        z2 = np.dot(w1,x) + b1 #hidden layer input
+        a2 = self.activation(z2, "tanh") #hidden layer activation
+        z3 = np.dot(w2,a2) + b2 #output layer input
+        a3 = self.activation(z3, "sigmoid") #output value
+        err = y - a3
+        probs = self.softmax_batch(a3)
+#         print "probs", probs.shape, probs[0:4, 0:5]
+        preds = np.argmax(probs,axis=0)
+#         print y[0:5]
+#         print preds[0:5]
+        loss = np.sum(np.abs(err)) / (err.shape[0] * err.shape[1])
+        predict_error = 0
+        for i in range(len(preds)):
+#             if i < 5: print y[preds[i],i] != 1
+            predict_error += 1 if y[preds[i],i] != 1 else 0
+        predict_error /= len(preds)
+        return loss, predict_error
         
     def build_model(self, X, Y, nn_hdim, iterations=10000):
 
@@ -104,27 +130,19 @@ class NeuralNet:
         learningRate = 0.01 # learning rate for gradient descent
         reg_lambda = 0.00 # regularization strength
         nn_output_dim = Y.shape[0]
-        m_inv = 1/X.shape[1] #inverse of number of examples for scaling
 
         #model weights and biases
         w1 = 0.001*np.random.random( (nn_hdim, X.shape[0]) ) - 0.0005
         w2 = 0.001*np.random.random( (nn_output_dim, nn_hdim) ) - 0.0005
         b1 = np.zeros( (nn_hdim, 1) )
         b2 = np.zeros( (nn_output_dim, 1) )
-        
-        #gradients of weights and biases
-        dw1 = np.zeros( (nn_hdim, X.shape[0]) ) 
-        dw2 = np.zeros( (nn_output_dim, nn_hdim) )
-        db1 = np.zeros( (nn_hdim, 1) )
-        db2 = np.zeros( (nn_output_dim, 1) )
-        
-        losses = np.zeros(iterations)
+
+        losses = []
         
         for i in range(iterations):
             #choose a random example
             ind = np.random.randint(X.shape[1])
             x, y = X[:,[ind]], Y[:,[ind]]
-#             print x.shape, y.shape
     
             #Forward propagation
             if prt:
@@ -137,7 +155,7 @@ class NeuralNet:
                 print "z2"
                 print z2.shape
                 print z2[1:5,:]
-            a2 = self.activation(z2, "sigmoid") #hidden layer activation
+            a2 = self.activation(z2, "tanh") #hidden layer activation
             if prt:
                 print "a2"
                 print a2.shape
@@ -151,30 +169,28 @@ class NeuralNet:
             if prt:
                 print "a3"
                 print a3.shape
-            
             err = y - a3
             if prt:
                 print "a3"
                 print a3[1:5,:]
                 print "err"
                 print err[1:5,:]
-            loss = self.getLoss(a3,y,nn_output_dim) #sum of squared differences
-            losses[i] = loss
-            if i % 1000 == 0 or i <= 10: print ("i = ", i, "loss = ", loss)
+            if i % 1000 == 0 or i <= 10: 
+                loss, predict_error = self.getLoss_training(X, Y, w1, w2, b1, b2) #for all examples in a batch
+                print "iter: ", i, "loss and prediction error for batch: ", loss, predict_error
+                losses.append(loss)
 
             #Backpropagation
             nl_delta = - err * self.activation_delta(a3, "sigmoid") #output layer delta: a(1-a) for sigmoid, 1-a**2 for tanh
             if prt:
                 print "nl"
                 print nl_delta
-            l2_delta = np.dot(w2.T, nl_delta) * self.activation_delta(a2, "sigmoid") #hidden layer delta #transpose nl_delta?
+            l2_delta = np.dot(w2.T, nl_delta) * self.activation_delta(a2, "tanh") #hidden layer delta #transpose nl_delta?
             if prt:
                 print "l2 delta"
                 print l2_delta[1:5,:]
             dw2 = np.dot(nl_delta, a2.T)
             dw1 = np.dot(l2_delta, x.T)
-#             db2 = np.sum( nl_delta, axis = 0, keepdims = True)
-#             db1 = np.sum( l2_delta, axis = 0, keepdims = True)
             db2 = nl_delta
             db1 = l2_delta
         
@@ -184,10 +200,11 @@ class NeuralNet:
             b1 -= learningRate * db1
             b2 -= learningRate * db2
         
-            learningRate *= 0.9999
-#             lerningRate = 1 - i*0.00005
+#             learningRate *= 0.9999
+            lerningRate = 0.01 / (1 + 100*i/iterations)
         
         model = { 'w1': w1, 'b1': b1, 'w2': w2, 'b2': b2}
+        print losses
         return model, losses
 
     def classify(self):
@@ -197,13 +214,13 @@ class NeuralNet:
         X = np.array(self.vector).T #192 x N
         y = self.oneHotIt_angle(self.imageIds) #4 x N
         
-        iterations = 1000000
+        iterations = int(1e8)
         model, losses = self.build_model(X, y, self.hiddenCount, iterations = iterations)
         plt.plot(losses)
         self.saveModel(model)
+        quit()
         accuracy = getAccuracy_angle(self, X, self.imageIds, model)
         print ('Average Training Accuracy: ', accuracy)
-        quit()
 
         with open(self.testFile) as document:
             for image in document.read().split(Constants.NEW_LINE):
